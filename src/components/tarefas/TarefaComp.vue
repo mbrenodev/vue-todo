273485ad3d5517af0da7e6de99f5e2cbce799039
marcio-breno <mbreno.dev@gmail.com>
2022-09-20 11:30:56 -0300

    Como usar CSS dinâmico

---
+++ src/components/tarefas/TarefaComp.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <v-list-item 
+    :class="{ 'blue lighten-4': tarefa.concluido }"
+    @click="changeValue"
+    >
+      <template v-slot:default="{}">
+        <v-list-item-action>
+          <v-checkbox :input-value="tarefa.concluido"></v-checkbox>
+        </v-list-item-action>
+
+        <v-list-item-content>
+          <v-list-item-title
+          :class="{'text-decoration-line-through' : tarefa.concluido}"
+          >{{ tarefa.titulo }}</v-list-item-title>
+        </v-list-item-content>
+      </template>
+    </v-list-item>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["tarefa"],
+  data(){
+    return {
+      tarefaR: this.tarefa
+    }
+  },
+  methods: {
+    changeValue(){
+      this.tarefaR.concluido = !this.tarefaR.concluido
+    }
+  }
+};
+</script>
+
+<style>
+</style>

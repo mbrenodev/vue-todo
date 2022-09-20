60aaef65f538bb6e1b835056c003b01d8227af7a
marcio-breno <mbreno.dev@gmail.com>
2022-09-20 09:43:45 -0300

    Como usar Vue Router

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,40 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer
+    <!-- <v-navigation-drawer
       v-model="drawer"
       app
     >
-      <!--  -->
+
+    </v-navigation-drawer> -->
+    <v-navigation-drawer 
+      v-model="drawer" 
+      app
+    >
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6"> Vue.js </v-list-item-title>
+          <v-list-item-subtitle> Lista de Tarefas </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item 
+          v-for="item in items" 
+          :key="item.title"
+          :to="item.to" 
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
 
     <v-app-bar app>
@@ -14,13 +44,19 @@
     </v-app-bar>
 
     <v-main>
-      <!--  -->
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
 
 <script>
-  export default {
-    data: () => ({ drawer: null }),
-  }
+export default {
+  data: () => ({ 
+    drawer: null,
+    items: [
+          { title: 'Tarefas', icon: 'mdi-view-dashboard', to:'/' },
+          { title: 'Sobre', icon: 'mdi-help-box', to:'/sobre' },
+        ], 
+  }),
+};
 </script>

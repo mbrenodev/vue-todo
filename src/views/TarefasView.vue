0cc77417ebeb18cd0c187b766fbfbfff80d6965c
marcio-breno <mbreno.dev@gmail.com>
2022-09-20 10:56:45 -0300

    Adicionando tarefas, v-model e methods

--- src/views/TarefasView.vue
+++ src/views/TarefasView.vue
@@ -1,31 +1,64 @@
 <template>
   <div>
-    <v-list
-        flat
-        subheader
-      >
-
-      <v-list-item-group
-        v-model="settings"
-        multiple
+    <v-col
+      cols="12"
+    >
+      <v-text-field
+        v-model="campoInput"
+        label="Digite sua tarefa"
+        outlined
+        clearable
+        @keyup.enter="handleAddTarefa"
+      ></v-text-field>
+    </v-col>
+    <v-list flat subheader>
+      <v-list-item-group 
+        multiple 
         active-class=""
       >
-      <TarefaComp />
-      <TarefaComp />
-      <TarefaComp />
+        <div
+          v-for="tarefa, index in tarefas"
+          :key="index"
+        >
+          <TarefaComp 
+            :tarefa="tarefa"
+          />
+
+        </div>
       </v-list-item-group>
     </v-list>
   </div>
 </template>
 
 <script>
-import TarefaComp from '@/components/tarefas/TarefaComp.vue';
+import TarefaComp from "@/components/tarefas/TarefaComp.vue";
 
-  export default {
-    name: 'TarefasView',
+export default {
+  name: "TarefasView",
 
-    components: {
-      TarefaComp
-},
+  components: {
+    TarefaComp,
+  },
+  data() {
+    return {
+      campoInput: null,
+      tarefas: [
+        { titulo: "Ir ao supermercado", concluido: false },
+        { titulo: "Ir ao supermercado", concluido: false },
+        { titulo: "Ir ao supermercado", concluido: false },
+      ],
+    };
+  },
+  methods: {
+    handleAddTarefa(){
+      if(this.campoInput){
+        this.tarefas.push({
+          titulo: this.campoInput,
+          concluido: false
+        })
+        this.campoInput = null
+      }
+    }
   }
+};
 </script>

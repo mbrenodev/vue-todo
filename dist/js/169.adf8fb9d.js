cb89be9d005ecf42263e7d368c44f86ee56cc085
marcio-breno <mbreno.dev@gmail.com>
2022-09-29 12:09:27 -0300

    Preparando para deploy

---
+++ dist/js/169.adf8fb9d.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvue_todo"]=self["webpackChunkvue_todo"]||[]).push([[169],{6169:function(t,u,n){n.r(u),n.d(u,{default:function(){return i}});var e=function(){var t=this;t._self._c;return t._m(0)},s=[function(){var t=this,u=t._self._c;return u("div",{staticClass:"about"},[u("h1",[t._v("This is an about page")])])}],a=n(1001),l={},r=(0,a.Z)(l,e,s,!1,null,null,null),i=r.exports}}]);
+//# sourceMappingURL=169.adf8fb9d.js.map
